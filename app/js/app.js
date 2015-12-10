82c53690db52b0d90927055de6311ccbe60e92cf
pollitux <raul@tingsystems.com>
2015-12-09 20:08:20 -0600

    set ts-token on the headers

--- app/js/app.js
+++ app/js/app.js
@@ -6,11 +6,11 @@
      * @desc Update xsrf $http headers to align with Django's defaults
      */
     function Run($http) {
-        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
-        $http.defaults.xsrfCookieName = 'csrftoken';
+        $http.defaults.headers.common['TS-TOKEN'] = 'AjFPLkeDMc5InbYZE2gbQiUtzi5F7LxHJE0sPAPb';
     }
 
     function Routes($stateProvider, $urlRouterProvider) {
+
         $stateProvider
             .state('index', {
                 url: '/',
@@ -18,11 +18,21 @@
                 templateUrl: '/static/app/index.html',
                 controllerAs: 'home',
                 controller: 'HomeCtrl'
+            })
+            .state('home', {
+                url: '/',
+                views: {
+                    'content': {
+                        templateUrl: '/templates/home.html',
+                        controllerAs: 'home',
+                        controller: 'HomeCtrl'
+                    }
+                }
             });
         $urlRouterProvider.otherwise('/');
     }
 
-    angular.module('hq', ['ui.router', 'app.controllers'])
+    angular.module('annalise', ['ui.router', 'ts.controllers'])
         .config(Routes)
         .run(Run);
 
--- app/js/services.js
+++ app/js/services.js
@@ -1,8 +1,18 @@
 'use strict';
 
 (function () {
+
+    var siteId = '167b5b84-2df8-46b1-b1fa-af42ad6432de';
+    var baseUrl = 'http://localhost:8000/api/v1/site/' + siteId + '/';
+
+    function HomeSrv($resource) {
+        return $resource(baseUrl + 'posts');
+    }
+
     // Assign factory to module
-    angular.module('hq.services', ['ngResource']);
+    angular.module('ts.services', ['ngResource'])
+        .factory('HomeSrv', HomeSrv);
 
     // Inject factory the dependencies
+    HomeSrv.$inject = ['$resource'];
 })();
