6d9a788f359725c549f3b464876563890fe016bd
jars1991 <jassael1991@gmail.com>
2015-12-11 19:33:18 -0600

    init wow in custom js

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,11 @@
 /* Custom js */
 
-$('a[ui-sref]').click(function () {
-   $('#header-mainmenu').collapse('hide');
+$(document).ready(function () {
+    // wow init
+    new WOW().init();
+
+    // collapse the header main menu if <a> clicked
+    $('a[ui-sref]').click(function () {
+        $('#header-mainmenu').collapse('hide');
+    });
 });
