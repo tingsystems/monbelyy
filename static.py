0ddfac5c092455cbe9032ff171b29d581b2b1fdb
Dante Omar Arciga <darciga@tingsystems.com>
2015-12-14 18:32:13 -0600

    Cambio minimos en el script de deploy

--- static.py
+++ static.py
@@ -8,30 +8,26 @@ AWS_API_KEY = 'AKIAJ5322OE4UXMGOOCA'
 AWS_SECRET_KEY = '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'
 AWS_CF_S3_BUCKETS = ('prueba-ts',)
 AWS_S3_BUCKET = 'annalise-tingsystems'
-location_base = 'static/'
+location_base = 'prueba/'
 # destination directory name (on s3)
 location_js = location_base + 'js'
 location_img = location_base + 'img'
 location_css = location_base + 'css'
 location_fonts = location_base + 'fonts'
-templates_dir = 'temp/'
-static_dir = 'prueba/'
-failed = open('failers.log', 'w')
+temp_dir = 'temp/'
 aws_path = None
 
-abs_template_dir = os.path.abspath(templates_dir)
-
-print(abs_template_dir)
+abs_temp_dir = os.path.abspath(temp_dir)
 
 templates = []
-
+# Copy files from dist/ to temp/
 print('Copy files...')
 res = call(['find dist/ -type f -exec cp {} temp/ \;'], shell=True)
 print('CMD result: ' + str(res))
 print('End files...')
 
-for file in os.listdir(abs_template_dir):
-    templates.append(os.path.join(abs_template_dir, file))
+for file in os.listdir(abs_temp_dir):
+    templates.append(os.path.join(abs_temp_dir, file))
 
 print('Uploading files:')
 
@@ -56,12 +52,12 @@ for t in templates:
         # indicated path in aws
         k.key = aws_path
         k.set_contents_from_filename(os.path.abspath(t), cb=percent_cb, num_cb=10)
-    # elif '.jpg' or '.ico' or '.png' in file_extension:
-    #     aws_path = os.path.join(location_img, os.path.basename(t))
-    # elif '.otf' or '.eot' or '.svg' or '.woff' or '.woff2' in file_extension:
-    #     aws_path = os.path.join(location_fonts, os.path.basename(t))
-    # else:
-    #     print("No se subio " + os.path.basename(t))
-    #     print(os.path.basename(t))
+        # elif '.jpg' or '.ico' or '.png' in file_extension:
+        #     aws_path = os.path.join(location_img, os.path.basename(t))
+        # elif '.otf' or '.eot' or '.svg' or '.woff' or '.woff2' in file_extension:
+        #     aws_path = os.path.join(location_fonts, os.path.basename(t))
+        # else:
+        #     print("No se subio " + os.path.basename(t))
+        #     print(os.path.basename(t))
 
 print('Upload complete')
