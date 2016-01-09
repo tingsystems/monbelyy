7bf0e9a6fcfc39fde0a12b5f0fe543322e18d6d8
jars1991 <jassael1991@gmail.com>
2016-01-08 18:52:27 -0600

    add post image directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -20,10 +20,31 @@
         };
     }
 
+    function postImage() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                // observe the post image directive
+                attrs.$observe('postImage', function (value) {
+                    // convert the interpolated value to an object
+                    var obj = scope.$eval(value);
+                    // default image value
+                    attrs.$set('src', 'https://s3-us-west-2.amazonaws.com/annalise-tingsystems/static/img/post-default.jpg');
+                    if (obj.url) {
+                        // if there are url image add the src of the image
+                        attrs.$set('src', obj.url);
+                    }
+                })
+            }
+        }
+    }
+
     // create the module and assign controllers
     angular.module('ts.directives', [])
-        .directive('postLink', postLink);
+        .directive('postLink', postLink)
+        .directive('postImage', postImage);
 
     // inject dependencies to controllers
     postLink.$inject = [];
+    postImage.$inject = [];
 })();
