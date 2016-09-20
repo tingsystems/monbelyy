994f136cb6152e5ed1e7b044b3666d9da91cfeaa
Dante Omar Arciga <darciga@tingsystems.com>
2016-09-20 14:00:56 -0500

    Change emails and img urls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -128,7 +128,7 @@
                 self.detail = results;
                 $rootScope.post = self.detail;
                 if (!self.detail.urlImages.original) {
-                    self.detail.urlImages.original = 'http://www.viajescoral.com/img/img-default.jpg';
+                    self.detail.urlImages.original = 'http://www.ainoxher.com.mx/img/img-default.jpg';
                 }
                 self.isBlog = $filter('filter')(self.detail.categories, {'slug': 'blog'})[0];
                 self.isService = $filter('filter')(self.detail.categories, {'slug': 'servicios'})[0];
