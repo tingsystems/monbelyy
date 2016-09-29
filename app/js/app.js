c111b77f9d30187895f8daf467c2773b7d4c5b63
Dante Omar Arciga <darciga@tingsystems.com>
2016-09-28 19:32:14 -0500

    Change keys api

--- app/js/app.js
+++ app/js/app.js
@@ -125,7 +125,7 @@
             $('#header-mainMenu').collapse('hide');
         });
         // initialise google analytics
-        $window.ga('create', 'UA-53555832-36', '');
+        //$window.ga('create', 'UA-53555832-36', '');
         // do something when change state
         $rootScope.$on('$stateChangeSuccess', function (event) {
             $window.ga('send', 'pageview', $location.path());
