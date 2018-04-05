073d177f3d9c75a7f3b901483edf1db797dc9d45
Dante Omar Arciga <darciga@tingsystems.com>
2018-04-05 12:16:08 -0500

    fix img product detail

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -670,7 +670,7 @@
         self.sizes = [];
         self.ready = false;
         self.taxonomies = [];
-        self.params = {ordering:'name'};
+        self.params = {ordering: 'name'};
         self.changeParams = false;
 
         // get post by category
