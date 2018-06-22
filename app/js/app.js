e024d8ee8156472fbf45e18f5712f80f049af1c8
Dante Omar Arciga <darciga@tingsystems.com>
2018-06-22 10:17:29 -0500

    fix ng controler in login

--- app/js/app.js
+++ app/js/app.js
@@ -161,11 +161,11 @@
      */
     function Run($http, $rootScope, $state, $window, $location, TaxonomySrv, $anchorScroll, EntrySrv, $auth, $localStorage) {
         $rootScope.$state = $state;
-        // $rootScope.host = 'https://mercadomovil.com.mx';
-        $rootScope.host = 'http://' + $location.host();
+        $rootScope.host = 'https://mercadomovil.com.mx';
+        //$rootScope.host = 'http://' + $location.host();
         $rootScope.hostAnnalise = 'https://mercadomovil.com.mx';
         $rootScope.apiV = 'v1';
-        $rootScope.apiShop = 'v1';
+        $rootScope.apiShop = 'v3';
         $rootScope.projectId = '5d951cfe-9a49-4b05-8708-c680e205d246';
         $http.defaults.headers.common['PROJECT-ID'] = '5d951cfe-9a49-4b05-8708-c680e205d246';
         $rootScope.hidePriceLogin = false;
