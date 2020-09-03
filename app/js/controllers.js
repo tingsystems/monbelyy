1523d46152347d42eb9e84503aeff6b1a2d2b616
FenixOne <sandra@tingsystems.com>
2020-09-03 13:28:08 -0500

    ajuste d estilo en detalle de producto

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -29,6 +29,20 @@
             });
         });
 
+        EntrySrv.get({
+            taxonomies: 'promociones-home',
+            isActive: 'True',
+            pageSize: 5,
+            ordering: '-createdAt',
+            fields: 'attachments,title,link,slug,excerpt,content'
+        }).$promise.then(function (results) {
+            self.promoHome = results.results;
+            //get featureImage
+            angular.forEach(self.promoHome, function (obj, ind) {
+                obj.featuredImage = $filter('filter')(obj.attachments, {kind: 'featuredImage'})[0];
+            });
+        });
+
         var paramsProducts = {};
         paramsProducts.isActive = 'True';
         if($rootScope.showWeb){
